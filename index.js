4bce2fd1dee26e1a9ba6856d54422f59e83ae963
cckuang6319 <cckuang6319@gmail.com>
2023-11-22 22:51:47 +0800

    no message

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ let video = document.getElementById("video");
 let canvas = document.getElementById("canvas");
 let image = document.getElementById("source");
 let result = document.getElementById("result");
+
 let worker = null;
 let videoWidth = window.innerWidth;
 let videoHeight = 0;
 let streaming = false;
+let cameraList = [];
+let defaultCamera = -1;
+let currCamera = -1;
 
 
 video.setAttribute("width", videoWidth);
 //video.setAttribute("height", 400);
 
-canvas.setAttribute("width", videoWidth);
+//canvas.setAttribute("width", videoWidth);
 //canvas.setAttribute("height", 400);
 
-image.setAttribute("width", videoWidth);
+//image.setAttribute("width", videoWidth);
 //image.setAttribute("height", 400);
 
 const rectangles = [
   {
     left: 0,
     top: 0,
-    width: 1600,
-    height: 1600,
+    width: 3840,
+    height: 2160,
   },
   /*
   {
@@ -35,8 +39,6 @@ const rectangles = [
   */
 ];
 
-cametaInitial();
-
 video.addEventListener(
     "canplay",
     (ev) => {
@@ -45,9 +47,11 @@ video.addEventListener(
   
         //video.setAttribute("width", width);
         video.setAttribute("height", videoHeight);
-        //canvas.setAttribute("width", width);
-        canvas.setAttribute("height", videoHeight);
 
+        canvas.setAttribute("width", video.videoWidth);
+        canvas.setAttribute("height", video.videoHeight);
+
+        image.setAttribute("width", video.videoWidth);
         image.setAttribute("height", videoHeight);
 
         streaming = true;
@@ -61,9 +65,11 @@ video.addEventListener(
     //const values = [];
 
     worker = await Tesseract.createWorker('chi_tra+eng');
+    /*
     await worker.setParameters({
         tessedit_char_whitelist: '',
     });
+    */
     /*
     for (let i = 0; i < rectangles.length; i++) {
         const { data: { text } } = await worker.recognize(image, { rectangle: rectangles[i] });
@@ -75,17 +81,24 @@ video.addEventListener(
     const { data: { text } } = await worker.recognize('en_zh.png');
     console.log(text);
     */
+    await cameraDeviceList();
+    cameraInitial();
+
     console.timeEnd();
     //await worker.terminate();
 })();
 
-async function cameraClick() {
+async function ocrClick() {
+
+    result.innerText = "Camera OCR Parsing...";
+
     // canvas
     const context = canvas.getContext("2d");
 
-    context.drawImage(video, 0, 0, videoWidth, videoHeight);
+    //context.drawImage(video, 0, 0, videoWidth, videoHeight);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-    const data = canvas.toDataURL("image/png");
+    const data = canvas.toDataURL("image/jpeg", 1.0);
     image.setAttribute("src", data);
     //image.setAttribute("src", "01.png");
 
@@ -99,15 +112,83 @@ async function cameraClick() {
     }
 }
 
-function cametaInitial() {
+function cameraInitial() {
+
+    let str = ''
+    cameraList.forEach(item => {
+        str = str + `${item.label},`
+    })
+    result.innerText = `Device:(${currCamera})\n${str}`;
+
+    console.log('deviceId:', cameraList[currCamera].deviceId)
     navigator.mediaDevices
-    .getUserMedia({ video: {facingMode: { exact: "environment" }} })
+    //.getUserMedia({ video: {facingMode: { exact: "environment" }} })
+    .getUserMedia( { video: { deviceId: { exact: cameraList[currCamera].deviceId } } })
     .then((stream) => {
         video.srcObject = stream;
         video.play();
     })
     .catch((err) => {
         console.error(`An error occurred: ${err}`);
+        result.innerText = `An error occurred: ${err}\nDevice:(${currCamera})\n${cameraList[currCamera].deviceId}\n${cameraList[currCamera].label}`
     });
+}
+
+async function cameraDeviceList() {
+    let devices = (await navigator.mediaDevices.enumerateDevices())
+    .filter(({ kind }) => kind === 'videoinput')
+
+    for(let idx=0; idx < devices.length; idx++) {
+        if(devices[idx].label.indexOf('back') > -1) {
+            cameraList.push({key: idx, label: `Camera${idx+1} (Back)`, deviceId:`${devices[idx].deviceId}`})
+            if(defaultCamera < 0) {
+                defaultCamera = idx
+                currCamera = idx
+            }
+        } else if(devices[idx].label.indexOf('front') > -1) {
+            cameraList.push({key: idx, label: `Camera${idx+1} (Front)`, deviceId:`${devices[idx].deviceId}`})
+        } else {
+            cameraList.push({key: idx, label: `Camera${idx+1} (Normal)`, deviceId:`${devices[idx].deviceId}`})
+            if(defaultCamera < 0) {
+                defaultCamera = idx
+                currCamera = idx
+            }
+        }
+    }
+
+    if(defaultCamera < 0) {
+        defaultCamera = 0;
+        currCamera = 0;
+    }
+}
+
+function cameraSwitch() {
+    currCamera = currCamera + 1
+    if(currCamera >= cameraList.length) {
+        currCamera = defaultCamera
+    }
+    
+    video.pause();
+    video.srcObject.getTracks()[0].stop();
+    video.srcObject = null;
+
+    cameraInitial()
+}
+
+const inputElement = document.getElementById("myfile");
+inputElement.addEventListener("change", handleFiles, false);
+
+async function handleFiles() {
+    result.innerText = "File OCR Parsing...";
+    try {
+        const selectedFile = inputElement.files[0];
+        image.src = window.URL.createObjectURL(selectedFile);
+        console.log('path:', selectedFile)
+        const { data: { text } } = await worker.recognize(image);
+        result.innerText = text;
+    } catch(err) {
+        result.innerText = err;
+    }
+
 }
 
